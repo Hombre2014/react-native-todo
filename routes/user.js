a6ea91c9f85d5aab310db9e8f2e43d9c814e3aa4
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-04-03 22:08:52 -0400

    Upload profile image

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,42 @@ const {
   userValidation,
   validateUserSignIn,
 } = require('../middleware/validation/user');
+
+const User = require('../models/user');
+
 const { isAuth } = require('../middleware/auth');
+
+const multer = require('multer');
+const sharp = require('sharp');
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb('Invalid image file', false);
+  }
+};
+
+const uploads = multer({ storage, fileFilter });
 
 router.post('/create-user', validateUserSignUp, userValidation, createUser);
 router.post('/sign-in', validateUserSignIn, userValidation, userSignIn);
-router.post('/create-post', isAuth, (req, res) => {
-  res.send('Welcome to create post page!');
+router.post('/upload-profile', isAuth, uploads.single('profile'), async (req, res) => {
+  const { user } = req;
+  if (!user) return res.status(401).json({ success: false, message: 'Unauthorized access!' });
+
+  try {
+    const profileBuffer = req.file.buffer;
+    const { width, height } = await sharp(profileBuffer).metadata();
+    const avatar = await sharp(profileBuffer).resize(Math.round(width / 2), Math.round(height / 2)).toBuffer();
+    await User.findByIdAndUpdate(user._id, { avatar });
+    res.status(201).json({ success: true, message: 'Profile picture uploaded successfully!' });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Server Error! Please, try again later.' });
+    console.log(error);
+  }
 });
 
 module.exports = router;
